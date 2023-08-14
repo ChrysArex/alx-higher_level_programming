4880c4f005ffe3a7f9cbbf29575471647f967429
root <root@localhost.localdomain>
2023-08-14 16:18:18 +0100

    task 3, reverse fixed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     idx = len(my_list) - 1
     for i in my_list:
-        print(int('{}'.format(my_list[idx])))
+        print(int('{:d}'.format(my_list[idx])))
         idx -= 1
