4dcfd66d6bb99316d35c7a69d82db09d9c0e684d
root <root@localhost.localdomain>
2023-08-14 16:41:38 +0100

    task 6, int conversion fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         if len(i) <= 0:
             continue
         for j in i[:len(i) - 1]:
-            print(int("{:n}".format(j)), end="")
+            print(int("{:d}".format(j)), end="")
             print(' ', end="")
-        print(int("{:n}\n".format(i[(len(i)-1)])))
+        print(int("{:d}\n".format(i[(len(i)-1)])))
