32978cc30028b754ad62f0d3a6d4387ed3241fe9
root <root@localhost.localdomain>
2023-08-28 23:33:20 +0100

    task 0-8 done

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    result = 0
+    try:
+        result = fct(*args)
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
+    else:
+        return result
