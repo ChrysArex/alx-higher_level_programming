c8da7ea6c1bbf5be0df6e7684ce9b19893ddbca6
root <root@localhost.localdomain>
2023-08-29 22:13:33 +0100

    test

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""empty class
+
+This module define an empty class Square that define a square.
+"""
+
+
+class Square:
+    """Empty class Square
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+This folder is an introduction of oriented object programming in python.
+It contain .py files that use severales OOP features of the python laguage.
+
