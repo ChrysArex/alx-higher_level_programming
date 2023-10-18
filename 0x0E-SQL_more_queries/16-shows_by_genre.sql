e4ea7aaf82015a9ef3a0f29fd3664cef2fcdbbc9
root <root@localhost.localdomain>
2023-10-19 00:14:34 +0100

    fix: display NULL If a show doesn’t have a genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- ists all shows, and all genres linked to that show in hbtn_0d_tvshows
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title, tv_genres.name FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_genres.name ASC;
+SELECT tv_shows.title, tv_genres.name FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_genres.name ASC;
