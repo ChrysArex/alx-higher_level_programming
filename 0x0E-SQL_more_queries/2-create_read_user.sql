8830aeeeaf2553a07ccefe17ce6bbf7a00c6f683
root <root@localhost.localdomain>
2023-10-19 00:33:45 +0100

    all mandatory tasks done

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- creates the database hbtn_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates the user user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY "user_0d_2_pwd";
 -- Grants the SELECT privilege in the database hbtn_0d_2 to user_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
