7c535e5d182e4c0d008037c562eb278306c0f8d8
root <root@localhost.localdomain>
2023-11-20 00:34:17 +0100

    update task8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                 sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).first().all()
-    if len(state) != 0:
+    state = session.query(State).first()
+    if state:
         print(str(state.id) + ": " + state.name)
     else:
         print("Nothing")
